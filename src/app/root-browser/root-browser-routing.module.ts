43b36c95a11a1a0aa013e44d7ab16a94ead05414
dkn1ght23 <mujammal.salman@gmail.com>
2022-03-24 20:22:54 +0600

    New components

--- src/app/root-browser/root-browser-routing.module.ts
+++ src/app/root-browser/root-browser-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {DashboardComponent} from "./components/dashboard/dashboard.component";
+import {Error404Component} from "./components/error404/error404.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent
+  },
+  {
+    path: '**',
+    component: Error404Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/root-browser/root-browser.module.ts
+++ src/app/root-browser/root-browser.module.ts
@@ -4,11 +4,15 @@ import { CommonModule } from '@angular/common';
 import { RootBrowserRoutingModule } from './root-browser-routing.module';
 import { RootDefaultComponent } from './components/root-default/root-default.component';
 import {BrowserModule} from "@angular/platform-browser";
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { Error404Component } from './components/error404/error404.component';
 
 
 @NgModule({
   declarations: [
-    RootDefaultComponent
+    RootDefaultComponent,
+    DashboardComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
