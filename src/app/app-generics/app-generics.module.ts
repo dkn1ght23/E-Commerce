0e1282437de1908648a5aefb3de3c9e399210bec
dkn1ght23 <mujammal.salman@gmail.com>
2022-03-25 19:00:45 +0600

    Top nav

--- src/app/app-generics/app-generics.module.ts
+++ src/app/app-generics/app-generics.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TopNavComponent } from './components/top-nav/top-nav.component';
 import {MatDividerModule} from "@angular/material/divider";
+import {RouterModule} from "@angular/router";
 
 
 
@@ -11,7 +12,8 @@ import {MatDividerModule} from "@angular/material/divider";
   ],
   imports: [
     CommonModule,
-    MatDividerModule
+    MatDividerModule,
+    RouterModule
   ],
   exports: [
     TopNavComponent
